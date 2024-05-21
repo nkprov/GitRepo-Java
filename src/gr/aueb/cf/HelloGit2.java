1df167f9eb7e7848ce21ccad6e6a4d3675c7dadd
nkprov <nprov@hcmr.gr>
2024-05-21 20:55:29 +0300

    Test change

--- src/gr/aueb/cf/HelloGit2.java
+++ src/gr/aueb/cf/HelloGit2.java
@@ -2,6 +2,6 @@ package gr.aueb.cf;
 
 public class HelloGit2 {
     public static void main(String[] args) {
-        System.out.println("Hello GIt 2");
+        System.out.println("Hello GIt 2 TEST");
     }
 }
